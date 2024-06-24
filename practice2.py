7e0ba357bf5a036502471ced3f9f7a2758a2b841
Anindya Sarder <134484183+Anindya-coder@users.noreply.github.com>
2024-06-24 14:04:42 +0600

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,81 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * (5/9)
+
+def celsius_to_kelvin(celsius):
+    return celsius + 273
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273
+
+def main():
+    while True:
+        print("Temperature Converter")
+        print("1: Celsius to fahrenheit")
+        print("2: Fahrenheit to celsius")
+        print("3: Celsius to Kelvin")
+        print("4: Kelvin to celsius")
+        print("q: to quit")
+
+        choice = input("Choice an option").strip().lower()
+
+        if choice == "1":
+            temp = float(input("Enter your temperature in celsius"))
+            try:
+                celsius = temp
+                fahrenheit = celsius_to_fahrenheit(celsius)
+                print(f"your temperature is {fahrenheit} fahrenheit")
+            except ValueError:
+                print("Enter a valid number")
+
+        elif choice == "2":
+            temp = float(input("Enter your temperature in fahrenheit"))
+            try:
+                fahrenheit = temp
+                celsius = fahrenheit_to_celsius(celsius)
+                print(f"Your temperature is {celsius} celsius")
+            except ValueError:
+                print("Enter a valid number")
+            
+        elif choice == "3":
+            temp = float(input("Enter your temperature in celsius"))
+            try:
+                celsius = temp
+                kelvin = celsius_to_kelvin(celsius)
+                print(f"your temperature is {kelvin} kelvin")
+            except ValueError:
+                print("Enter a valid number")
+
+        elif choice == "4":
+            temp = float(input("Enter your temperature in kelvin"))
+            try:
+                kelvin = temp
+                celsius = kelvin_to_celsius(kelvin)
+                print(f"your temperature is {celsius} celsius")
+            except ValueError:
+                print("Enter a valid number")
+
+        elif choice == "q":
+            break
+
+        else:
+            print("Enter a valid number")
+
+        print()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+        
+
+
+                
