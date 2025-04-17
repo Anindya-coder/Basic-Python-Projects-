99ce2888e9144b54afac4675bbea2b524708d7b7
Anindya Sarder <134484183+Anindya-coder@users.noreply.github.com>
2025-04-17 19:40:48 +0600

    pattern.py

--- pattern.py
+++ pattern.py
@@ -64,3 +64,98 @@ for i in range(4):
     for j in range(i+1):
         print(i+1,end=' ')
     print()
+#pattern 6
+#1 
+#2 3 
+#4 5 6 
+#7 8 9 10 
+
+i=0 
+j=0
+num =1
+for i in range(4):
+    for j in range(i+1):
+        print(num,end=' ')
+        num+=1
+    print()
+    
+#pattern 7
+#1
+#1 2
+#1 2 3
+#1 2 3 4
+
+i,j=0,1
+for i in range(4):
+    for j in range(i+1):
+        j+=1
+        print(j,end=' ')
+    print()
+
+#pattern 8
+#1
+#2 1
+#3 2 1
+#4 3 2 1
+
+i=0
+j=0
+for i in range(4):
+    for j in range(i+1):
+        print(i-j+1,end=' ')
+    print()
+
+#pattern 9
+#* * * *
+#* * * 
+#* *
+#*
+i,j=0,0
+for i in range(4):
+    for j in range(4-i):
+        print('*',end=' ')
+    for j in range(i):
+        print(end=' ')
+    print()
+
+#pattern 10 
+#1 1 1 1 
+# 2 2 2 
+#  3 3 
+#   4
+i,j=0,0
+for i in range(4):
+    for j in range(i):
+        print(end=' ')
+    for j in range(4-i):
+        print(i+1,end=' ')
+    print()
+    
+#pattern 11 
+#    1
+#   2 3
+#  4 5 6
+# 7 8 9 10
+i,j,num=0,0,1
+for i in range(4):
+    for j in range(4-i-1):
+        print(end=' ')
+    for j in range(i+1):
+        print(num,end=' ')
+        num+=1
+    print()
+
+#pattern 12 
+#    1
+#  1 2 1
+# 1 2 3 2 1
+#1 2 3 4 3 2 1 
+i,j=0,0
+for i in range(4):
+    for j in range(4-i-1):
+        print(end=' ')
+    for j in range(i+1):
+        print(j+1,end=" ")
+    for j in range(i):
+        print(i-j,end=' ')
+    print()
